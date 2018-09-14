1cbe663c8676e4d061358e18d8ab1e978897ec7b
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2018-09-14 14:27:10 -0500

    first demo project

---
+++ election/contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.2;
+
+contract Election{
+  //Store Candidate
+  //Read Candidate
+  string public candidate;
+  //Contructor
+  function Election() public {
+    candidate = "Ryan McCreadie";
+
+  }
+}
